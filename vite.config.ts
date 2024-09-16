5ae76362ed2b008535d71305d730c1cb1ce96356
Peter Ott <peter.ott@solarpunk.buzz>
2024-09-16 13:47:14 +0200

    node polyfills

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig(({mode}) => {
@@ -9,7 +10,15 @@ export default defineConfig(({mode}) => {
       'process.env.DEVCON6_SESSSIONS_HASH': JSON.stringify(env.DEVCON6_SESSSIONS_HASH) ?? JSON.stringify(''),
       'process.env.BEE_API_URL': JSON.stringify(env.BEE_API_URL) ?? JSON.stringify('http://localhost:1633/')
     },
-    plugins: [react()],
+    plugins: [
+      react(),
+      nodePolyfills(),
+    ],
     base: '/DevconAgora/',
+    resolve: {
+      alias: {
+        stream: 'stream-browserify',
+      },
+    },
   }
 })
